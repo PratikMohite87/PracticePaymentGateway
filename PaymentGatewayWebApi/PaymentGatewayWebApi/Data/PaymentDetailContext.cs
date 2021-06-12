9457f92c4864ed9256273e877935f9a5411b772b
PratikMohite87 <mohitepratik48@gmail.com>
2021-06-12 23:03:54 +0530

    WebApi

---
+++ PaymentGatewayWebApi/PaymentGatewayWebApi/Data/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentGatewayWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGatewayWebApi.Data
+{
+    public class PaymentDetailContext : DbContext
+    {
+                                    // db provider and connection string is passed.
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        { }
+
+        public DbSet<PaymentDetail> PaymentDetails { set; get; }
+    }
+}
---
+++ PaymentGatewayWebApi/PaymentGatewayWebApi/Models/PaymentDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentGatewayWebApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { set; get; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { set; get; }
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { set; get; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { set; get; }
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { set; get; }
+    }
+}
